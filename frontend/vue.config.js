e85570fef7c6e9b31718b106d9121ef5d14339fb
Urij Sivunov <Sivunov_US@teleformis.ru>
2024-03-12 03:31:06 +0400

    start 11

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,4 @@
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 module.exports = {
   devServer: {
     proxy: {
@@ -6,5 +7,13 @@ module.exports = {
         changeOrigin: true,
       },
     },
+  },
+  chainWebpack: (config) => {
+    config.plugin("fork-ts-checker").tap((args) => {
+      let allowUseMem = 300;
+      // in vue-cli shuld args[0]['typescript'].memoryLimit
+      args[0]["typescript"].memoryLimit = allowUseMem;
+      return args;
+    });
   },
 };
