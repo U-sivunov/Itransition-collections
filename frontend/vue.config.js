46fe3c77134eb409e36058aec10cbef42d560e0b
Urij Sivunov <Sivunov_US@teleformis.ru>
2024-03-18 23:37:43 +0400

    versel routes

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,1 @@
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-module.exports = {
-  devServer: {
-    proxy: {
-      "/api/": {
-        target: "https://itransition-collections-back2-git-main-u-sivunovs-projects.vercel.app:3000",
-        changeOrigin: true,
-      },
-    },
-  },
-};
+
