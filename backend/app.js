fb12892b167311ec3d7ca87264b08f1a82778295
U-sivunov <sivunus@gmail.com>
2024-03-22 06:15:31 +0400

    Login

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,9 @@ const router = express.Router();
 
 app.use(cors({
     origin: '*',
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
