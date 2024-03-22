1f4992101e61ce50257fcc0a8e8fc601fbd24e15
U-sivunov <sivunus@gmail.com>
2024-03-22 05:45:49 +0400

    Login

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,4 +4,5 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://itransition-collections-back2.vercel.app/";
+axios.defaults.withCredentials = true;
 createApp(App).use(router).mount("#app");
