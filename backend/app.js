32d04078dadb0e0596858249a34a2c6876e46e33
Urij Sivunov <Sivunov_US@teleformis.ru>
2024-03-25 01:01:57 +0400

    prisma user create

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
 const cors = require('cors');
 const { PrismaClient } = require('@prisma/client');
 
