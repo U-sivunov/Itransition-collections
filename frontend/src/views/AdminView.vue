f8e1fbe0ce563098ddaa71efa6dd3682ac851dd0
U-sivunov <sivunus@gmail.com>
2024-03-26 04:28:45 +0400

    admin page

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -7,6 +7,9 @@
         <div class="admin-types">
             <h3>CollectionTypes</h3>
             <b-table hover :items="collectionTypes"></b-table>
+            <b-form @submit="addNewType" v-if="show">
+              <b-form-input v-model="newType" placeholder="Enter new collection type"></b-form-input>
+            </b-form>
         </div>
     </div>
 </template>
@@ -18,24 +21,32 @@
       return {
         collectionTypes: [],
         users: [],
+        newType: '',
       };
     },
     mounted() {
         console.log('moooo');
       axios
-          .get("/api/collectionTypes",            )
-          .then((res) => {
-              this.collectionTypes = res.data;
-              console.log(res);
-          });
+        .get("/api/collectionTypes",            )
+        .then((res) => {
+            this.collectionTypes = res.data;
+            console.log(res);
+        });
       axios.get("/api/users")
-          .then((res) => {
-              this.users = res.data;
-              console.log(res);
-          });
+        .then((res) => {
+            this.users = res.data;
+            console.log(res);
+        });
     },
     methods: {
-
+      addNewType() {
+        axios
+          .post("/api/collectionTypes",{name: this.newType})
+          .then((res) => {
+              this.newType = '';
+              this.mounted();
+          });
+    }
     },
   };
 </script>
