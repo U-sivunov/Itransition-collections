af5377e8296aa69704168884dd1a37f6d71e9ec0
Urij Sivunov <Sivunov_US@teleformis.ru>
2024-03-28 15:16:09 +0400

    collections

--- frontend/src/views/MyCollectionsView.vue
+++ frontend/src/views/MyCollectionsView.vue
@@ -7,9 +7,30 @@
         <b-form-input v-model="newCollectionName" placeholder="Enter new collection name"></b-form-input>
         <b-form-select v-model="newCollectionType" :options="collectionTypes"></b-form-select>
         <VMarkdownEditor v-model="newCollectionDescription" placeholder="Enter new collection description"></VMarkdownEditor>
+        <div class="block-name" >String parameters</div>
         <div v-for="n in stringFieldsNumber">
           <b-form-input class="string-field" placeholder="Parameter name"></b-form-input>
-          <b-button v-if="n === stringFieldsNumber" variant="primary" v-on:click="stringFieldsNumber++">Add</b-button>
+          <b-button v-if="n === stringFieldsNumber || stringFieldsNumber === 0" variant="primary" v-on:click="stringFieldsNumber++">Add</b-button>
+        </div>
+        <div class="block-name" >Text parameters</div>
+        <div v-for="n in textFieldsNumber">
+          <b-form-input class="text-field" placeholder="Parameter name"></b-form-input>
+          <b-button v-if="n === textFieldsNumber || textFieldsNumber === 0" variant="primary" v-on:click="textFieldsNumber++">Add</b-button>
+        </div>
+        <div class="block-name" >Boolean parameters</div>
+        <div v-for="n in booleanFieldsNumber">
+          <b-form-input class="boolean-field" placeholder="Parameter name"></b-form-input>
+          <b-button v-if="n === booleanFieldsNumber || booleanFieldsNumber === 0" variant="primary" v-on:click="booleanFieldsNumber++">Add</b-button>
+        </div>
+        <div class="block-name" >Number parameters</div>
+        <div v-for="n in numberFieldsNumber">
+          <b-form-input class="number-field" placeholder="Parameter name"></b-form-input>
+          <b-button v-if="n === numberFieldsNumber || numberFieldsNumber === 0" variant="primary" v-on:click="numberFieldsNumber++">Add</b-button>
+        </div>
+        <div class="block-name" >Date parameters</div>
+        <div v-for="n in dateFieldsNumber">
+          <b-form-input class="date-field" placeholder="Parameter name"></b-form-input>
+          <b-button v-if="n === dateFieldsNumber || dateFieldsNumber === 0" variant="primary" v-on:click="dateFieldsNumber++">Add</b-button>
         </div>
 
         <b-button type="submit" variant="primary">Create collection</b-button>
@@ -28,7 +49,11 @@
                 newCollectionName: '',
                 newCollectionType: '',
                 newCollectionDescription: '',
-                stringFieldsNumber: 2,
+                stringFieldsNumber: 1,
+                textFieldsNumber: 1,
+                booleanFieldsNumber: 1,
+                numberFieldsNumber: 1,
+                dateFieldsNumber: 1,
                 collectionTypes: []
             };
         },
@@ -48,12 +73,27 @@
             addNewCollection() {
                 const stringFields = event.target.getElementsByClassName('string-field');
                 const stringFieldsArray = [...stringFields].map(f => f.value);
-                console.log(stringFieldsArray);
+
+                const textFields = event.target.getElementsByClassName('text-field');
+                const textFieldsArray = [...stringFields].map(f => f.value);
+
+                const booleanFields = event.target.getElementsByClassName('boolean-field');
+                const booleanFieldsArray = [...stringFields].map(f => f.value);
+
+                const numberFields = event.target.getElementsByClassName('number-field');
+                const numberFieldsArray = [...stringFields].map(f => f.value);
+
+                const dateFields = event.target.getElementsByClassName('string-field');
+                const dateFieldsArray = [...stringFields].map(f => f.value);
                 const newCollection = {
                     title: this.newCollectionName,
                     description: this.newCollectionDescription,
                     collectionType: this.newCollectionType,
                     stringFieldNames: stringFieldsArray,
+                    textFieldNames: textFieldsArray,
+                    booleanFieldNames: booleanFieldsArray,
+                    numberFieldNames: numberFieldsArray,
+                    dateFieldNames: dateFieldsArray
                 }
                 axios
                     .post("/api/collection",newCollection)
