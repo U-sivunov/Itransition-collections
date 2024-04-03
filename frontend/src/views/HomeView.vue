0110bd2a1cf2bddc6e4d8ec9314dec91aee4be62
Urij Sivunov <Sivunov_US@teleformis.ru>
2024-04-03 14:53:40 +0400

    cards

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,41 +1,53 @@
 <template>
+  <wordcloud
+    :data="defaultWords"
+    nameKey="name"
+    valueKey="value"
+    :color="myColors"
+    :showTooltip="true"
+    :wordClick="wordClickHandler">
+  </wordcloud>
   <div class="home">
     <div class="recent-items">
       <item-component v-for="item in resentItems" :item="item">
 
       </item-component>
+      <div v-for="item in resentItems">
+        {{item}}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import wordcloud from 'vue-wordcloud'
+  import axios from "axios";
+  import ItemComponent from "@/components/ItemComponent";
 
-import axios from "axios";
-import ItemComponent from "@/components/ItemComponent";
-
-export default {
-  components: {
-      ItemComponent
-  },
-  data() {
-    return {
-      resentItems: [],
-    };
-  },
-  mounted() {
-    this.getResentItems();
-  },
-  methods: {
-    getResentItems() {
-      axios
-      .get("/api/get-resent-items/")
-      .then((res) => {
-        this.recentItems = res.data;
-      })
-      .catch( e => {
-          console.log(e)
-      });
+  export default {
+    components: {
+        ItemComponent,
+        wordcloud
+    },
+    data() {
+      return {
+        resentItems: [],
+      };
+    },
+    mounted() {
+      this.getResentItems();
+    },
+    methods: {
+      getResentItems() {
+        axios
+        .get("/api/get-resent-items/")
+        .then((res) => {
+          this.recentItems = res.data;
+        })
+        .catch( e => {
+            console.log(e)
+        });
+      }
     }
   }
-}
 </script>
